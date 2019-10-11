376c610da570d72ee7c83c635196ee6f90f8c42d
vebinua <vincentedwardgbinua@gmail.com>
2019-10-11 17:41:13 +0800

    created master template file for header and footer definitions. Removed primary content and placed on a separate view file (home.blade.php).

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,15 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-    <title>User Card - </title>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-    <link rel="stylesheet" href="assets/css/main.css" />
-    <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
-</head>
-<body class="is-preload">
+@extends('master')
+
+@section('title', 'User Card')
+
+@section('content')
+
 <div id="wrapper">
     <section id="main">
         <header>
-            <span class="avatar"><img src="images/users/<?=$user->id?>.jpg" alt="" /></span>
-            <h1></h1>
-            <p></p>
+            <span class="avatar"><img src="images/users/{{$user->id}}.jpg" alt="" /></span>
+            <h1>{{$user->name}}</h1>
+            <p>{{$user->comments}}</p>
         </header>
     </section>
     <footer id="footer">
@@ -21,13 +17,6 @@
             <li>&copy; Pictureworks</li>
         </ul>
     </footer>
-
 </div>
-<script>
-    if ('addEventListener' in window) {
-        window.addEventListener('load', function() { document.body.className = document.body.className.replace(/\bis-preload\b/, ''); });
-        document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
-    }
-</script>
-</body>
-</html>
+
+@endsection('content')
