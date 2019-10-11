d3c827c8b87edecbe3297e32ac3ea122b3e6eedb
vebinua <vincentedwardgbinua@gmail.com>
2019-10-11 18:57:37 +0800

    removed test template. reviewed final codebase for cleanup with inline comments/styles.

--- app/Users.php
+++ app/Users.php
@@ -25,18 +25,5 @@ class Users extends Model
     $comments .= $new_comments;
 
     Users::where('id', $id)->update(['comments' => $comments]);
-
-    /*$result = mysqli_query($mydb, 'SELECT comments FROM users WHERE id = "'.mysqli_real_escape_string($mydb, $id).'"');
-    if(mysqli_error($mydb))
-        apidie('DB Error: '.mysqli_error($mydb), 500);
-    if(mysqli_num_rows($result) <= 0)
-        apidie('No such user (1)', 404);
-
-    $row = mysqli_fetch_object($result);
-    $row->comments .= "\n".$comments;
-
-    mysqli_query($mydb, 'UPDATE users SET comments = "'.mysqli_real_escape_string($mydb, $row->comments).'"');
-    return mysqli_error($mydb);*/
   }
-
 }
